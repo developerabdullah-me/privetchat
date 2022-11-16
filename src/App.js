3400ae8bcfc79205c82c34affa2aa3a67fcf4f6e
unknown <abdullahtalha9999@gmail.com>
2022-11-16 15:13:28 +0600

    m commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Register from './pages/Register';
+import './style.scss'
+
+function App() {
+  return (
+    <div>
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useNavigate, Link } from "react-router-dom";
+const Register = () => {
+    return (
+        <div className="formContainer">
+        <div className="formWrapper">
+         <h1>hello</h1>
+      </div>
+      </div>
+    );
+};
+
+export default Register;
